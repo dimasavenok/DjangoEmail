83e41cc04096ac0ecb05870d097709d66d4e9e4c
dimasavenok <dimas.savenok1990@gmail.com>
2025-09-11 20:10:02 +0300

    mainapp models

---
+++ mainapp/models.py
@@ -0,0 +1,53 @@
+from django.utils import timezone
+
+from django.db import models
+
+# Create your models here.
+class Recipient(models.Model):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.email} | {self.full_name}"
+
+class Message(models.Model):
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.subject
+
+class Mailing(models.Model):
+    STATUS_CREATED = "created"
+    STATUS_RUNNING = "running"
+    STATUS_FINISHED = "finished"
+    STATUS_CHOICES = [
+        (STATUS_CREATED, "Создана"),
+        (STATUS_RUNNING, "Запущена"),
+        (STATUS_FINISHED, "Завершена"),
+    ]
+
+    start_at = models.DateTimeField()
+    end_at = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CREATED)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    recipients = models.ManyToManyField(Recipient)
+
+    def __str__(self):
+        return f"Рассылка {self.pk} - {self.message.subject}"
+
+class MailingAttempt(models.Model):
+    STATUS_SUCCESS = "success"
+    STATUS_FAILED = "failed"
+    STATUS_CHOICES = [
+        (STATUS_SUCCESS, "Успешно"),
+        (STATUS_FAILED, "Не успешно"),
+    ]
+
+    attempted_at = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_FAILED)
+    response_server = models.TextField(null=True, blank=True)
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
+
+
